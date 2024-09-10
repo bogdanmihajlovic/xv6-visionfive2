05cf9db187522d7bb778052648eea5a8416268b4
Bogdan Mihajlovic <bogdanmihajlovic4@gmail.com>
2024-09-10 21:09:52 +0200

    Main thread executes shell

--- kernel/main.c
+++ kernel/main.c
@@ -1,20 +1,24 @@
 #include "types.h"
 #include "param.h"
-// #include "memlayout.h"
+#include "memlayout.h"
 #include "riscv.h"
 #include "defs.h"
 
 volatile static int started = 0;
 
+
 // start() jumps here in supervisor mode on all CPUs.
+
 void __attribute__((aligned(4)))main()
 {
   if(cpuid() == 1){
     consoleinit();
     printfinit();
-    printf("\n");
-    printf("xv6 kernel is booting\n");
-    printf("\n");
+    printf("\r\n");
+    printf("xv6 kernel is booting\r\n");
+    printf("\r\n");
+    shell();
+
     // kinit();         // physical page allocator
     // kvminit();       // create kernel page table
     // kvminithart();   // turn on paging
@@ -27,7 +31,7 @@ void __attribute__((aligned(4)))main()
     // iinit();         // inode table
     // fileinit();      // file table
     // virtio_disk_init(); // emulated hard disk
-    // userinit();      // first user process
+    //userinit();      // first user process
     __sync_synchronize();
     started = 1;
   } else {
@@ -36,10 +40,10 @@ void __attribute__((aligned(4)))main()
       ;
     __sync_synchronize();
     printf("hart %d starting\n", cpuid());
-    //kvminithart();    // turn on paging
-    //trapinithart();   // install kernel trap vector
-    //plicinithart();   // ask PLIC for device interrupts
+    // kvminithart();    // turn on paging
+    // trapinithart();   // install kernel trap vector
+    // plicinithart();   // ask PLIC for device interrupts
   }
 
-  //scheduler();        
+  scheduler();        
 }
